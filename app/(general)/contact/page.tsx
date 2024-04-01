6931948cb703c8b991a6c17c75895f8d51643cee
Jesus Alberto Rivera Mina <jariveram@uao.edu.co>
2024-04-01 08:36:34 -0500

    firts commit

---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+ title: 'Contact Page',
+ description: 'Información de contacto',
+};
+export default function ContactPage() {
+
+    return (
+        <>
+        <span className="text-7xl">Contact Page</span>
+        </>
+    )
+}
---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+
+
+
+export const metadata: Metadata = {
+ title: 'Pricing Page',
+ description: 'Pagina de precios de mis servicios',
+};
+export default function PricingPage() {
+
+
+    return (
+        <>
+        <span className="text-7xl">Pricing Page</span>
+        </>
+    )
+}
