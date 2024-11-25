79df4c07c0026293cefc7f56f26c01f4c79addc5
Jesus Alberto Rivera Mina <jariveram@uao.edu.co>
2024-11-25 16:14:02 -0500

    test

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { HomeIcon } from "@primer/octicons-react";
 import Link from "next/link";
 import { ActiveLink } from "../active-link/ActiveLink";
+import axios from "axios";
 
 const navItems = [
   { path: "/about", text: "About" },
@@ -9,12 +12,28 @@ const navItems = [
 ];
 
 const Navbar = () => {
+
+
+  const handleClick = async () => {
+    axios
+      .get("https://betaasistencia.uao.edu.co/")
+      .then(function (response) {
+        // manejar respuesta exitosa
+        console.log(response);
+      })
+      .catch(function (error) {
+        // manejar error
+        console.log(error);
+      });
+  };
   return (
     <nav className="flex bg-blue-800 bg opacity-3 p-2 m-2 rounded">
       <Link href={"/"} className="flex items-center">
         <HomeIcon className="mr-2" />
         <span>Home</span>
       </Link>
+
+      <button onClick={handleClick}>Peticion</button>
 
       <div className="flex flex-1"></div>
       {navItems.map((navItem) => (
